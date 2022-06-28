3aebcd919ad6fc29140e5581bb717913be2fc10a
val1983by <106666688+val1983by@users.noreply.github.com>
2022-06-28 20:49:54 +0300

    Урок 1
    Урок 1 добавление на github

---
+++ General.java
@@ -0,0 +1,77 @@
+public class General {
+    public static void main(String[] args) {
+
+        //1
+//        int a = 2;
+//        int p = 4 * a;
+//        System.out.println("P= " + p);
+        //2
+//        int a = 4;
+//        int s = a*a;
+//        System.out.println("S= " + s);
+        //3
+//        int a = 2;
+//        int b = 4;
+//        int s = a * b;
+//        int p = 2 * (a + b);
+//        System.out.println("S= " + s);
+//        System.out.println("P= " + p);
+        //4
+          final double pi = 3.14;
+//        int d = 5;
+//        double l = pi * d;
+//        System.out.println("L= " + l);
+
+        //5
+//        int a = 4;
+//        int v = a*a*a;
+//        int s = 6*a*a;
+//        System.out.println("V=" + v);
+//        System.out.println("S=" + s);
+
+        //6
+//        int a = 4;
+//        int b = 5;
+//        int c = 6;
+//        int v = a * b * c;
+//        int s = 2 * (a*b + b*c + a*c);
+//        System.out.println("V= " + v);
+//        System.out.println("S= " + s);
+
+        //7
+//        int r = 4;
+//        double l = 2 * pi * r;
+//        double s = pi * r * r;
+//        System.out.println("L= " + l);
+//        System.out.println("S= " + s);
+
+        //8
+        int a = 3;
+        int b = 6;
+        double sr = (a + b)/2.0;
+        System.out.println("Sr= " + sr);
+
+//        //9
+//        int a = 4;
+//        int b = 6;
+//        System.out.println(math.sqr(a+b));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
