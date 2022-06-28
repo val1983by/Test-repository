6558fa77248a0a2fe8d36823327b318ee956345a
val1983by <106666688+val1983by@users.noreply.github.com>
2022-06-28 21:51:03 +0300

    Add files via upload

--- General.java
+++ General.java
@@ -46,15 +46,96 @@ public class General {
 //        System.out.println("S= " + s);
 
         //8
-        int a = 3;
-        int b = 6;
-        double sr = (a + b)/2.0;
-        System.out.println("Sr= " + sr);
+//        int a = 3;
+//        int b = 6;
+//        double sr = (a + b)/2.0;
+//        System.out.println("Sr= " + sr);
 
-//        //9
+        //9
 //        int a = 4;
 //        int b = 6;
-//        System.out.println(math.sqr(a+b));
+//        System.out.println(Math.sqrt(a+b));
+
+        //10
+//        int a = 5;
+//        int b = 7;
+//        int sum = a*a + b * b;
+//        int razn = a*a - b * b;
+//        int proizv = a * a * b * b;
+//        double chast = (double)(a * a ) / (b * b);
+//        System.out.println("sum= " + sum);
+//        System.out.println("razn= " + razn);
+//        System.out.println("proizv= " + proizv);
+//        System.out.println("chastn= " + chast);
+
+//        //11
+//        int a = 4;
+//        int b = 5;
+//        int c = Math.abs(a) + Math.abs(b);
+//        System.out.println("Sum= " + c);
+//        c = Math.abs(a) - Math.abs(b);
+//        System.out.println("Razn= " + c);
+//        c = Math.abs(a) * Math.abs(b);
+//        System.out.println("Proizv= " + c);
+//        double d = (double)Math.abs(a) / Math.abs(b);
+//        System.out.println("Chastn= " + d);
+
+        //12
+//        int a = 4;
+//        int b = 6;
+//        double c = Math.sqrt((double)a*a + b*b);
+//        double p = a + b + c;
+//        System.out.println("C= " + c);
+//        System.out.println("P= " + p);
+//
+//        //13
+//        int r1 = 16;
+//        int r2 = 10;
+//        double s1 = pi * r1 * r1;
+//        double s2 = pi * r2 * r2;
+//        double s3 = s1 - s2;
+//        System.out.println("s1= " + s1);
+//        System.out.println("s2=" + s2);
+//        System.out.println("s3=" + s3);
+//
+//        //14
+//        int l=24;
+//        double r = l/(2*pi);
+//        double s = pi * r;
+//        System.out.println("r=" + r);
+//        System.out.println("s=" + s);
+
+        //15
+        double s = 20;
+        double d=Math.sqrt(4*s/(pi));
+        double l = pi * d;
+
+        //16
+        int x1 = 10;
+        int x2 = 20;
+        int rast = x2 - x1;
+        System.out.println("Расстояние междуй x1 и x2" + rast);
+
+        //22
+        int a = 4;
+        int b = 6;
+        System.out.println("a= " + a + " b= " + b);
+        int c;
+        c = a;
+        a = b;
+        b = c;
+        System.out.println("a= " + a + " b= " + b);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
